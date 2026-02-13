118298098790851ab89402d12940524bf4b8d6f1
BGH <pkpkpp87@gmail.com>
2026-02-13 10:58:30 +0900

    resource.h에 IDP_SOCKETS_INIT_FAILED 누락 복구

--- resource.h
+++ resource.h
@@ -3,26 +3,26 @@
 // ChatServer.rc에서 사용되고 있습니다.
 //
 #define IDD_CHATSERVER_DIALOG           102
+#define IDP_SOCKETS_INIT_FAILED         103
 #define IDR_MAINFRAME                   128
-#define IDD_SETTING_DLG                 135
+#define IDD_SETTING_DLG                 130
 #define IDC_BTN_SEND                    1002
 #define IDC_LIST_CHAT                   1004
 #define IDC_EDIT_INPUT                  1005
 #define IDC_BTN_SET                     1006
-#define IDC_MEDBR_LOGPATH               1018
-#define IDC_IPADDR                      1019
-#define IDC_CBXEX_DELDAY                1020
-#define IDC_BTN_SAVE                    1021
-#define BTN_CANCEL                      1022
-#define IDC_BTN_CANCEL                  1022
+#define IDC_IPADDR                      1007
+#define IDC_CBXEX_DELDAY                1010
+#define IDC_MEDBR_LOGPATH               1013
+#define IDC_BTN_SAVE                    1014
+#define IDC_BTN_CANCEL                  1015
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1023
+#define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
